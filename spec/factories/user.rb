7f9a254fb99d34bdb46294ae433825c1e7f8e155
YuuTakamine <kasiwaba.20220302@icloud.com>
2022-05-04 21:57:14 +0900

    ユーザー管理機能

---
+++ spec/factories/user.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nick_name              {'test'}
+    email                 {'test@example'}
+    password              {'000000'}
+    password_confirmation {password}
+    last_name             {'高橋'}
+    last_name_kana        {'タカハシ'}
+    first_name            {'悠太'}
+    first_name_kana       {'ユウタ'}
+    birthday              {'2001年3月24日'}
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,37 +1,18 @@
 require 'rails_helper'
  RSpec.describe User, type: :model do
+
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
    describe "ユーザー新規登録" do
-     it "nick_nameが空だと登録できない" do
+    it 'nick_name:必須' do
       @user.nick_name = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      binding.pry
+      expect(@user.errors.full_messages).to include("can't be brank")
+    end
      it "emailが空では登録できない" do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
      end
-     it "passwordが空では登録できない" do
-      @user.password = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
-     end
-     it 'passwordとpassword_confirmationが不一致では登録できない' do
-      @user.password = '123456'
-      @user.password_confirmation = '1234567'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-     end
-     it 'emaiは@を含まないと登録できない' do
-      @user.email = 'testmail'
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Email is invalid')
-     end
-     it 'passwordが5文字以下では登録できない' do
-      @user.password = '00000'
-      @user.password_confirmation = '00000'
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
-     end
-     
    end
  end
